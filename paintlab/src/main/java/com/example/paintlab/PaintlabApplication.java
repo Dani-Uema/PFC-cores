c2da81e5a8dfee63147fbb77d320cf622bc20ddb
Dani-Uema <126253432+Dani-Uema@users.noreply.github.com>
2025-09-14 20:00:55 -0300

    feat:first commit

---
+++ paintlab/src/main/java/com/example/paintlab/PaintlabApplication.java
@@ -0,0 +1,13 @@
+package com.example.paintlab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaintlabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaintlabApplication.class, args);
+	}
+
+}
